edf3f96c9ca915ef19ac19d73930f766590c4d25
Nick De Jaeger <nick.de.jaeger@tenforce.com>
2018-06-08 10:05:26 +0200

    update grid system

--- scripts/squares.js
+++ scripts/squares.js
@@ -1,12 +1,12 @@
 var keepItemsSquared = (function keepItemsSquaredFunction() {
   $('.c-item').each(function() {
 
-    if ( $(window).width() >= 992 ) {
+    if ( $(window).width() >= 768 ) {
 
       $(this).css({
         'height': $(this).width() + 'px'
       });
-      
+
     }
 
   });
