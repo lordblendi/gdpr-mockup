65ea98c356780927480083f417d61c6183d8eff5
Nóra Szepes <nora.szepes@tenforce.com>
2018-06-13 15:16:36 +0200

    commenting nick's js code

--- scripts/squares.js
+++ scripts/squares.js
@@ -1,16 +1,13 @@
+// functinality to keep the items squared
+// if the width is changing, the height should change too
 var keepItemsSquared = (function keepItemsSquaredFunction() {
   $('.c-item').each(function() {
-
     if ( $(window).width() >= 768 ) {
-
       $(this).css({
         'height': $(this).width() + 'px'
       });
-
     }
-
   });
-
   return keepItemsSquaredFunction;
 }());
 
