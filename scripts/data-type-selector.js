70bfbf6c772ec7ddd7cfa99a1b026c7c8097acf3
Nóra Szepes <nora.szepes@tenforce.com>
2018-06-22 14:09:18 +0200

    Being able to set the data-type

--- scripts/data-type-selector.js
+++ scripts/data-type-selector.js
@@ -1,8 +1,14 @@
 ---
 ---
 
+/*
+  making sure to add a tick to the selected item
+*/
 $('.JS_dataTypeSelector').on('click', function() {
   $('body').addClass('selector-data-types--visible');
+
+  // remove old selected tick
+  $('.JS_showSelected').remove();
 
   const currentChosenText = $(this).find('.JS_text').html();
   const possibleDataTypes = $('#multiSelector-data-types .JS_text');
@@ -17,11 +23,17 @@ $('.JS_dataTypeSelector').on('click', function() {
   });
 });
 
+$('#multiSelector-data-types .JS_filterableCell').on('click', function(){
+  closeDataTypeSelectorUntouched();
+  const newText = $(this).find('.JS_text').html();
+  $('.JS_dataTypeSelector .JS_text').html(newText);
+});
+
 /*
   close the popup and make sure the overlay is still there just without the darkness
 */
 function closeDataTypeSelectorUntouched() {
   $('body').removeClass('selector-data-types--visible');
-  $('.multiSelector--visible').removeClass('multiSelector--visible');
+  $('.multiSelector--visible').removeClass('multiSelector--visible animate-popup animate-fadein');
   openOverlay();
 }
