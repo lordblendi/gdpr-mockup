8ac1d8d7561d21c3edbe71bd82be8351e7941c9a
Nóra Szepes <nora.szepes@tenforce.com>
2018-06-25 13:16:41 +0200

    Added resize v 0.1

--- scripts/comments.js
+++ scripts/comments.js
@@ -1,15 +1,19 @@
 function setupCommentToggle() {
-  $('.JS_comments-toggle').on('click', function(){
+  resizeTextarea('.JS_autosize');
+
+  $('.JS_comments-toggle').on('click', function() {
     const toggle = $(this);
     const details = toggle.closest('.JS_item-details');
     $(details).toggleClass('panel-comments--visible');
 
-    if($(details).hasClass('panel-comments--visible')) {
+    if ($(details).hasClass('panel-comments--visible')) {
       readCommentsFunctionality();
+      resizeTextarea('.JS_autosize');
     }
+
     // always redraw charts to make sure it has the good width
     // also making sure that the transition has already finished
-    setTimeout(function () {
+    setTimeout(function() {
       drawChart();
     }, 100);
   });
@@ -19,5 +23,18 @@ function setupCommentToggle() {
 function readCommentsFunctionality() {
   $('.JS_read').on('click', function() {
     $(this).addClass('is-read');
+  });
+}
+
+function resizeTextarea(selector) {
+  // setting initial height
+  setTimeout(function() {
+    $(selector).css('height', 'auto');
+    $(selector).height($(selector)[0].scrollHeight);
+  }, 100);
+
+  $(selector).off('keyup').on('keyup', function() {
+    $(this).css('height', 'auto');
+    $(this).height(this.scrollHeight);
   });
 }
