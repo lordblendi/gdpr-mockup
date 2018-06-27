38c2d9fb0c219dc91ace0f53b635e3b3f5904ba1
Nóra Szepes <nora.szepes@tenforce.com>
2018-06-27 11:34:02 +0200

    added view selector

---
+++ scripts/view-selector.js
@@ -0,0 +1,13 @@
+$('.JS_viewSelector').on('click', function() {
+  const viewSelector = $(this);
+  const dataType = $(viewSelector).attr('data-view');
+  if (viewSelector.hasClass('JS_selectorItem--active')) {
+    if (dataType === "overview") {
+      $('.JS_c-list').addClass('view--overview')
+      $('.JS_c-list').removeClass('view--risk')
+    } else if (dataType === "risk") {
+      $('.JS_c-list').addClass('view--risk')
+      $('.JS_c-list').removeClass('view--overview')
+    }
+  }
+});
